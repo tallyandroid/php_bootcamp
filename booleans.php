e8e34a07a2aa26be6ae37ba079e9bebd12f31e6c
Christian Tisby <cduane82@hotmail.com>
2020-01-15 01:43:07 -0500

    adding booleans

--- booleans.php
+++ booleans.php
@@ -1,3 +1,17 @@
 <?php
 $logged_in = true;
 $is_admin = false;
+$data = null;
+
+$user_id = null;
+$my_id; // null
+
+//  These are not the same
+$boolean = true;
+$string = "true";
+
+$name = null;
+$string = "null";
+
+//  This is how you pass in multiple values to var_dump()
+var_dump($logged_in, $data);
