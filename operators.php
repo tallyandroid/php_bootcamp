913d695cbbd7c7dbd7ad54eba920af9a06340593
Christian Tisby <cduane82@hotmail.com>
2020-01-15 01:56:56 -0500

    adding logical operators

--- operators.php
+++ operators.php
@@ -1,2 +1,38 @@
 <?php
 
+$count = 10;
+$size = 2;
+
+var_dump($count + 5);
+var_dump($count * size);
+
+
+// $price = 2.95;
+// $quantity = 5;
+
+var_dump($price * $quantity);
+
+//string concatenation
+$message = "Hello";
+$name = "Dave";
+
+echo $message . " " . $name;
+
+
+// type conversion or type juggling
+// the "150" in the variable price will be converted to an integer
+$price = "150";
+$quantity = 3;
+
+$price = $price * $quantity;
+
+var_dump($price);
+
+//logical operator example
+
+$is_editor = true;
+$is_adming = false;
+
+var_dump(! $is_editor);
+
+var_dump($is_editor and $is_admin);
