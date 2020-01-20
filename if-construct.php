4e7bdc661271c9e8df9190906032aab32efeb30d
Christian Tisby <cduane82@hotmail.com>
2020-01-19 23:58:59 -0500

    adding if/else

---
+++ if-construct.php
@@ -0,0 +1,12 @@
+<?php
+//The if construct
+$articles = [];
+
+if (empty($articles)) {
+    echo "The array is empty";
+} else {
+    echo "The array is not empty";
+}
+//If the condition was false, the code block would not be entered.
+//Else extends an if statement
+
